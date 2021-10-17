810044276a355e7da5374a8385acaad2af449f0f
crypt0box <pray.happy76@gmail.com>
2021-10-17 14:55:57 +0900

    feat: スターをクリックしてレーティングを変化させられるよう実装

--- src/StarRating.tsx
+++ src/StarRating.tsx
@@ -1,13 +1,20 @@
-import { FaStar } from "react-icons/fa";
+import { useState } from "react";
+import Star from "./Star";
 
-export default function StarRating() {
+export default function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(3);
   return (
     <>
-      <FaStar color="red" />
-      <FaStar color="red" />
-      <FaStar color="red" />
-      <FaStar color="grey" />
-      <FaStar color="grey" />
+      {[...Array(totalStars)].map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
     </>
   );
 }
