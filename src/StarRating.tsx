e443f0db96261e2268c97b4ca5e02107388bac0c
crypt0box <pray.happy76@gmail.com>
2021-10-17 14:16:40 +0900

    initial commit

---
+++ src/StarRating.tsx
@@ -0,0 +1,13 @@
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating() {
+  return (
+    <>
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="grey" />
+      <FaStar color="grey" />
+    </>
+  );
+}
